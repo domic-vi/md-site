ee2a4f09d86626d6ad920019eff83c6fd0ed721c
Victor Biloshenko <domic.php@gmail.com>
2018-10-22 20:51:29 +0300

    template: table style

--- lib/template.php
+++ lib/template.php
@@ -11,12 +11,38 @@
 body {
 	font-size: 11pt;
 }
+
 pre>code {
 	background-color: #eee;
 	border: 1px solid gray;
 	padding: 10px;
 	display: inline-block;
 }
+
+table {
+	border-spacing: 0;
+}
+
+th {
+	border-top: 2px solid black;
+	text-align: center;
+}
+
+td, th {
+	border-left: 1px solid black;
+	border-bottom: 1px solid black;
+	padding: 5px 8px;
+	margin: 0;
+}
+
+td {
+	text-align: left;
+}
+
+table tr>*:last-child {
+	border-right: 1px solid black;
+} 
+
 </style>
 </head>
 <body>
